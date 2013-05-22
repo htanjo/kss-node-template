5e011ef8209142e90695b2b087eaa7f1613bbe40
Tanjo, Hiroyuki <hiroyuki.tanjo@mail.rakuten.com>
2013-05-22 19:46:37 +0900

    Update to 0.1.3

--- public/main.js
+++ public/main.js
@@ -1,12 +1,14 @@
 (function ($) {
     $(function () {
 
-        var $menu = $('.kss-menu'),
+        var $sidebar = $('.kss-sidebar'),
+            $menu = $('.kss-menu'),
             $childMenu = $('.kss-menu-child'),
             $menuItem = $menu.find('.kss-menu-item'),
             $childMenuItem = $childMenu.find('.kss-menu-item'),
             ref = $menu.data('kss-ref');
 
+        // Dynamic menu activation
         function scrollSpy() {
             var scrollTop = $(window).scrollTop(),
                 $anchors = $childMenu.find('a'),
@@ -26,14 +28,33 @@
             }
         }
 
+        // Fix sidebar position when window is large
+        function fixSidebar() {
+            if ($sidebar.outerHeight() <= $(window).height()) {
+                $sidebar.addClass('kss-fixed');
+            }
+            else {
+                $sidebar.removeClass('kss-fixed');
+            }
+        }
+
+        // Activate current page item
         $menuItem.eq(ref).addClass('kss-active');
 
+        // Append child menu and attach scrollSpy
         if ($childMenu.length) {
             $childMenu.show().appendTo($menuItem.eq(ref));
             $(window).scroll(scrollSpy);
             scrollSpy();
         }
 
+        // Sidebar position
+        if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+            fixSidebar();
+            $(window).resize(fixSidebar);
+        }
+
+        // Syntax hightlignting with Rainbow.js
         $('code.html').attr('data-language', 'html');
         $('code.css').attr('data-language', 'css');
         $('code.less, code.scss').attr('data-language', 'generic');
